7bde4b0a8dbec1ccc043685d9ef7a74a3ceb42f5
Rumeysa Layık <37456977+rumeysalyk@users.noreply.github.com>
2021-08-26 21:30:38 +0300

    Add files via upload

---
+++ examples/Example3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Example3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = 13195 ;
+            bool flag = true;
+            int max=0;
+
+            for(int i=2;i<number;i++)
+            {
+                if(number%i==0)
+                {
+                    for(int j=2;j<i;j++)
+                    {
+                        if(i%j==0)
+                        {
+                            flag = false;
+                            break;
+                        }
+    
+                    }
+                    if(flag==true)
+                        max = i;
+                }
+            }
+
+            Console.WriteLine(max);
+        }
+    }
+}
