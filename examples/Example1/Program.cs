bf8a958922b024386ff00c95cf0c8b0aea6f852b
Rumeysa Layık <37456977+rumeysalyk@users.noreply.github.com>
2021-08-26 12:22:00 +0300

    Add files via upload

---
+++ examples/Example1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number =10,sum=0;
+            
+            for(int i=0;i<number;i++)
+            {
+
+                if(i%15==0)
+                {
+                    sum +=i;
+                    continue;
+                }
+                else if(i%3 == 0)
+                    sum +=i;
+                else if(i%5 == 0)
+                    sum +=i;
+            }
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
